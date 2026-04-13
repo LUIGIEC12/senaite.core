c6ba6c17fc7c731da047181a13eb3f4ee7ff135f
LUIGIGM <131417983+LUIGIEC12@users.noreply.github.com>
2026-04-12 23:01:45 -0500

    Update model_c111.py

--- src/senaite/core/exportimport/instruments/cobas_c111/model_c111/model_c111.py
+++ src/senaite/core/exportimport/instruments/cobas_c111/model_c111/model_c111.py
@@ -2,12 +2,9 @@
 
 from bika.lims import bikaMessageFactory as _
 from senaite.core.i18n import translate as t
-from senaite.core.exportimport.instruments import IInstrumentImportInterface
 
 from .parser import CobasC111Parser
 from .importer import CobasC111Importer
-
-from zope.interface import implementer
 
 import json
 import traceback
@@ -16,61 +13,55 @@ import traceback
 title = "Cobas C111"
 
 
-@implementer(IInstrumentImportInterface)
-class cobas_c111(object):
-    """Cobas C111 Import Interface"""
+def Import(context, request):
 
-    def __init__(self, context):
-        self.context = context
+    infile = request.form.get('cobas_c111_file', None)
+    fileformat = request.form.get('cobas_c111_format', None)
+    instrument = request.form.get('instrument', None)
 
-    def __call__(self):
-        return self
+    errors = []
+    logs = []
+    warns = []
 
-    def Import(self, context, request):
+    parser = None
 
-        infile = request.form.get('cobas_c111_file', None)
-        fileformat = request.form.get('cobas_c111_format', None)
-        instrument = request.form.get('instrument', None)
-
-        errors = []
-        logs = []
-        warns = []
-
-        parser = None
-
-        if not infile:
-            errors.append(_("No file selected"))
-            return json.dumps({
-                'errors': errors,
-                'log': logs,
-                'warns': warns
-            })
-
-        # 👉 Define formatos soportados
-        if fileformat == 'astm':
-            parser = CobasC111Parser(infile)
-        else:
-            errors.append(_("Formato no soportado"))
-
-        if parser:
-            importer = CobasC111Importer(
-                parser=parser,
-                context=context,
-                override=[False, False],
-                instrument_uid=instrument
-            )
-
-            try:
-                importer.process()
-            except Exception:
-                errors.append(traceback.format_exc())
-
-            errors = importer.errors
-            logs = importer.logs
-            warns = importer.warns
-
+    if not infile:
+        errors.append(_("No file selected"))
         return json.dumps({
             'errors': errors,
             'log': logs,
             'warns': warns
         })
+
+    # 👉 FORMATO SOPORTADO
+    if fileformat == 'astm':
+        parser = CobasC111Parser(infile)
+    else:
+        errors.append(_("Formato no soportado"))
+
+    if parser:
+        importer = CobasC111Importer(
+            parser=parser,
+            context=context,
+            override=[False, False],
+            instrument_uid=instrument
+        )
+
+        try:
+            importer.process()
+        except Exception:
+            errors.append(traceback.format_exc())
+
+        errors = importer.errors
+        logs = importer.logs
+        warns = importer.warns
+
+    return json.dumps({
+        'errors': errors,
+        'log': logs,
+        'warns': warns
+    })
+
+
+# 🔥 ESTO ES LO MÁS IMPORTANTE
+cobas_c111 = Import
